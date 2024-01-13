a5ff2f1de49cce41e268aac06dd56dbf0c466bfc
RanjanKumar93 <rk0936626@gmail.com>
2024-01-13 22:49:56 +0530

    updating proj

--- oo.js
+++ oo.js
@@ -1,25 +1,36 @@
 const http = require("http");
 const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
   const url = req.url;
   const method = req.method;
-
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input name="masai" type ="text" /><button type="submit" >Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input name="message" type ="text"><button type="submit" >Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "ROHJAN");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
